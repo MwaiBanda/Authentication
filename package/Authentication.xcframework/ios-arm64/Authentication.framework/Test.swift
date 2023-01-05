12c8811bb7bb7e915ecb9c0376b65ae0b4129292
Mwai Banda <mwai.banda@cbn.org>
2023-01-05 15:34:46 -0600

    Added test swift

---
+++ package/Authentication.xcframework/ios-arm64/Authentication.framework/Test.swift
@@ -0,0 +1,15 @@
+
+import Foundation
+import 'Authentication.h'
+
+class SwiftClass {
+    func test() {
+        Authentication.shared.extendedFunc()
+    }
+}
+
+extension Authentication {
+    func extendedFunc() -> {
+
+    }
+}
