18f4b8ba463bd7c059b9ec72f4d8156b4d9a1214
Mwai Banda <mwai.banda@cbn.org>
2023-01-06 10:02:18 -0600

    added xc framework

---
+++ package/Authentication/Sources/Authentication-Swift/authenticate.swift
@@ -0,0 +1,9 @@
+import Authentication
+
+public struct authenticate {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+        Authentication.shared.service
+    }
+}
--- package/Authentication/Sources/Authentication/Authentication.swift
+++
@@ -1,6 +0,0 @@
-public struct authentication {
-    public private(set) var text = "Hello, World!"
-
-    public init() {
-    }
-}
